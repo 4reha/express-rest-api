d9897f09bbbdc47cc0dc81ecf8cf8f7da2b7ca93
4reha <ael-hadd@student.1337.ma>
2023-02-02 15:41:42 +0100

    fixing port

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,10 @@
 import * as dotenv from 'dotenv';
-import {networkInterfaces} from 'os';
 import app from './server';
 
 dotenv.config();
-const net = networkInterfaces();
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, ()=>{
-  console.log(`Server is running on http://${net.wlp3s0[0].address}:${port}`);
+  console.log(`Server is running on ${port}`);
 });
